a9d45279ca826e974f3ddd633e3c16c65dffc374
Banjo Victor <victorolaolubanjo@yahoo.com>
2023-03-11 08:43:09 +0100

    firebase-configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {initializeApp, provideFirebaseApp } from '@angular/fire/app'
+import { environment } from 'src/environment/env';
 
 
 @NgModule({
@@ -13,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environment/env.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBQKGJPUsOzp3rXjfaez-psLozxGvUM4EU",
+        authDomain: "netflix-clone-27e1f.firebaseapp.com",
+        projectId: "netflix-clone-27e1f",
+        storageBucket: "netflix-clone-27e1f.appspot.com",
+        messagingSenderId: "905494752157",
+        appId: "1:905494752157:web:0c2dbb54e0007c77791d74",
+        measurementId: "G-4ZJXJGLT5W"
+    }
+}
